a2810fc355b4c22b2603e7116756495ab6609bf1
sanazez <sanazez1234@gmail.com>
2021-01-17 14:36:02 +0300

    feat: change properties card-component

--- src/components/Main/Card/CardContainer.jsx
+++ src/components/Main/Card/CardContainer.jsx
@@ -14,12 +14,12 @@ function countDays(creatureDate) {
 let mapStateToProps = (state, props) => {
   if (state.headerElement.searchResults && state.headerElement.searchResults.length) {
     return {
-      logo: state.headerElement.searchResults[props.cardIndex].company_logo,
-      company: state.headerElement.searchResults[props.cardIndex].company,
+      logo: state.headerElement.searchResults[props.cardIndex].company_logo_url,
+      company: state.headerElement.searchResults[props.cardIndex].company_name,
       title: state.headerElement.searchResults[props.cardIndex].title,
-      type: state.headerElement.searchResults[props.cardIndex].type,
-      location: state.headerElement.searchResults[props.cardIndex].location,
-      created: countDays(state.headerElement.searchResults[props.cardIndex].created_at)
+      type: state.headerElement.searchResults[props.cardIndex].job_type,
+      location: state.headerElement.searchResults[props.cardIndex].candidate_required_location,
+      created: countDays(state.headerElement.searchResults[props.cardIndex].publication_date)
     }
   }
   return {}
