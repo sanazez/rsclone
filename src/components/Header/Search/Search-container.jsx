2f7b858d13e661f6a3016d3b1d5c0a2e8a77b7b3
HannaHerman <mrs.hanna.herman@gmail.com>
2021-01-22 21:54:41 +0300

    feat: add pagination routing

--- src/components/Header/Search/Search-container.jsx
+++ src/components/Header/Search/Search-container.jsx
@@ -7,10 +7,11 @@ import * as axios from 'axios';
 
 class SearchContainer extends React.Component {
     componentDidMount() {
-        axios.get(`http://localhost:9000/`)
-            .then(res => {
-                this.props.setJobs(res.data.items);
-            })
+        // axios.get(`http://localhost:9000/`)
+        //     .then(res => {
+        //         console.log(res.data);
+        //         this.props.setJobs(res.data.items);
+        //     })
         // axios.get(`http://localhost:9000/hh`)
         //     .then(res => {
         //         this.props.setJobs(res.data.items);
@@ -21,8 +22,8 @@ class SearchContainer extends React.Component {
         axios.get(`http://localhost:9000/testAPI/search?search=${this.props.searchText}`)
             .then(res => {
                 if (res.data.items && res.data.items.length) {
-                    console.log(res.data.items)
-                    this.props.setJobs(res.data.items)
+                    console.log(res.data);
+                    this.props.setJobs(res.data.items, res.data.pages);
                 }
             })
     }
@@ -45,8 +46,8 @@ const mapDispatchToProps = (dispatch) => {
             let action = updateSearchTextActionCreater(text);
             dispatch(action);
         },
-        setJobs: (jobs) => {
-            let action = loadAllActionCreater(jobs);
+        setJobs: (jobs, pages) => {
+            let action = loadAllActionCreater(jobs, pages);
             dispatch(action)
         }
     }
